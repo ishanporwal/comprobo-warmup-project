14acc5dcf26d655ea374eb4b9dab178897cee571
Ishan Porwal <ishanporwaltennis@gmail.com>
2026-09-13 17:46:58 -0400

    Add finite state controller

---
+++ src/warmup_project/src/finite_state_controller.cpp
@@ -0,0 +1,141 @@
+#include <memory>
+#include <functional>
+#include <chrono>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "neato2_interfaces/msg/bump.hpp"
+
+class FiniteStateController : public rclcpp::Node {
+public:
+    FiniteStateController()
+        : Node("finite_state_controller"),
+          obstacle_detected_state_(false),
+          bump_detected_state_(false),
+          neato_state_("move_forward")
+    {
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100),
+            std::bind(&FiniteStateController::run_loop, this)
+        );
+        cmd_vel_publisher_ =
+            this->create_publisher<geometry_msgs::msg::Twist>(
+                "/cmd_vel",
+                10
+            );
+        bump_subscriber_ =
+            this->create_subscription<neato2_interfaces::msg::Bump>(
+                "/bump",
+                10,
+                std::bind(
+                    &FiniteStateController::bump_callback,
+                    this,
+                    std::placeholders::_1
+                )
+            );
+        scan_subscriber_ =
+            this->create_subscription<sensor_msgs::msg::LaserScan>(
+                "/scan",
+                10,
+                std::bind(
+                    &FiniteStateController::scan_callback,
+                    this,
+                    std::placeholders::_1
+                )
+            );
+    }
+
+private:
+    void bump_callback(
+        const neato2_interfaces::msg::Bump::ConstSharedPtr msg
+    ) {
+        if (
+            msg->left_front ||
+            msg->right_front ||
+            msg->left_side ||
+            msg->right_side
+        ) {
+            bump_detected_state_ = true;
+        }
+    }
+
+    void scan_callback(
+        const sensor_msgs::msg::LaserScan::ConstSharedPtr msg
+    ) {
+        if (msg->ranges.empty()) {
+            return;
+        }
+        double distance_front = msg->ranges[0];
+        if (distance_front > 0.1 && distance_front < 0.3) {
+            obstacle_detected_state_ = true;
+        }
+        else {
+            obstacle_detected_state_ = false;
+        }
+    }
+
+    void move_forward() {
+        geometry_msgs::msg::Twist command;
+        command.linear.x = 0.1;
+        command.angular.z = 0.0;
+        cmd_vel_publisher_->publish(command);
+    }
+
+    void rotate() {
+        geometry_msgs::msg::Twist command;
+        command.linear.x = 0.0;
+        command.angular.z = -0.1;
+        cmd_vel_publisher_->publish(command);
+    }
+
+    void stop() {
+        geometry_msgs::msg::Twist command;
+        command.linear.x = 0.0;
+        command.angular.z = 0.0;
+        cmd_vel_publisher_->publish(command);
+    }
+
+    void run_loop() {
+        if (bump_detected_state_) {
+            neato_state_ = "stop";
+        }
+        if (neato_state_ == "move_forward") {
+            move_forward();
+
+            if (obstacle_detected_state_) {
+                neato_state_ = "rotate";
+            }
+        }
+        else if (neato_state_ == "rotate") {
+            rotate();
+
+            if (!obstacle_detected_state_) {
+                neato_state_ = "move_forward";
+            }
+        }
+        else {
+            stop();
+        }
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr
+        cmd_vel_publisher_;
+    rclcpp::Subscription<neato2_interfaces::msg::Bump>::SharedPtr
+        bump_subscriber_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
+        scan_subscriber_;
+    bool obstacle_detected_state_;
+    bool bump_detected_state_;
+    std::string neato_state_;
+};
+
+int main(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<FiniteStateController>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
